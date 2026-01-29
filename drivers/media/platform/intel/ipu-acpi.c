bb27c00515e2b9c5b039cef4e95c5de27da23299
Khai Wen, Ng <khai.wen.ng@intel.com>
2026-01-29 17:42:32 +0800

    ipu-acpi: Add ACPI ID for Sensing ISX031
    Sensing ISX031 have additional connection to their gpio.
    Hence treat it as a different hardware module, and differentiate
    with different ACPI ID.
    
    Signed-off-by: Khai Wen, Ng <khai.wen.ng@intel.com>

--- drivers/media/platform/intel/ipu-acpi.c
+++ drivers/media/platform/intel/ipu-acpi.c
@@ -108,6 +108,31 @@ static const struct ipu_acpi_devices supported_devices[] = {
 		},
 		.sensor_dt = MIPI_CSI2_TYPE_YUV422_8,
 	},
+	{
+		.hid_name = "INTC031S",
+		.real_driver = ISX031_NAME,
+		.get_platform_data = get_sensor_pdata,
+		.priv_data = NULL,
+		.priv_size = 0,
+		.connect = TYPE_SERDES,
+		.serdes_name = "max9x",
+		.sensor_physical_addr = ISX031_I2C_ADDRESS,
+		.link_freq = 1600,
+		.ser_physical_addr = 0x40,
+		.ser_gpio = {
+			{
+				.chip_hwnum = 0,
+				.con_id = "reset",
+				.flags = GPIO_ACTIVE_LOW,
+			},
+			{
+				.chip_hwnum = 7,
+				.con_id = "fsin",
+				.flags = GPIO_ACTIVE_LOW,
+			},
+		},
+		.sensor_dt = MIPI_CSI2_TYPE_YUV422_8,
+	},
 #endif
 #if IS_ENABLED(CONFIG_VIDEO_AR0820)
 	{ 
