44e599c46a769b9eeff5085d818f84e9605bbb6b
Yew, Chang Ching <chang.ching.yew@intel.com>
2026-03-04 13:46:53 +0800

    Fix kernelver parsing in POST_ADD script

--- dkms-kernel-source.sh
+++ dkms-kernel-source.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-vers=(${kernelver//./ })
-major="${vers[0]}"
-minor="${vers[1]}"
-
-makefile=/usr/src/linux-headers-${kernelver}/Makefile
-if [ $(wc -l < $makefile) -eq 1 ] && grep -q "^include " $makefile ; then
-  makefile=$(tr -s " " < $makefile | cut -d " " -f 2)
+if [[ -z "${kernelver:-}" ]]; then
+  kernelver="$(uname -r)"
 fi
-subver=$(grep "SUBLEVEL =" $makefile | tr -d " " | cut -d "=" -f 2)
 
-echo "Downloading kernel source $major.$minor.$subver for $kernelver"
-if [ "$subver" -ne 0 ]; then
-	kernelprefix=linux-$major.$minor.$subver
+major=$(echo "$kernelver" | cut -d- -f1| cut -d. -f1)
+minor=$(echo "$kernelver" | cut -d- -f1| cut -d. -f2)
+patch=$(echo "$kernelver" | cut -d- -f1| cut -d. -f3)
+
+if ! [[ "$major" =~ ^[0-9]+$ ]]; then major=0; fi
+if ! [[ "$minor" =~ ^[0-9]+$ ]]; then minor=0; fi
+if ! [[ "$patch" =~ ^[0-9]+$ ]]; then patch=0; fi
+
+echo "Downloading major $major minor $minor patch $patch"
+if (( patch != 0 )); then
+  kernelprefix="linux-$major.$minor.$patch"
 else
-	kernelprefix=linux-$major.$minor
+  kernelprefix="linux-$major.$minor"
 fi
 wget --no-check-certificate https://mirrors.edge.kernel.org/pub/linux/kernel/v$major.x/$kernelprefix.tar.xz -O $kernelprefix.tar.xz
 
 for arg in "$@"; do
     echo "Extracting: $kernelprefix/$arg"
     tar -xvf "$kernelprefix.tar.xz" "$kernelprefix/$arg" \
-      --xform="s,^${kernelprefix//./\\.}/,$major.$minor.$subver/,"
+      --xform="s,^${kernelprefix//./\\.}/,$major.$minor.$patch/,"
 done
